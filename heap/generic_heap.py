bca091b3e8e1e311419c465b66d7f6a780ec05e4
ryanboris <ryan.boris@gmail.com>
2019-07-25 15:49:23 -0400

    :sparkles: Complete generic heap

--- heap/generic_heap.py
+++ heap/generic_heap.py
@@ -1,22 +1,47 @@
 class Heap:
-    def __init__(self, comparator):
+    def __init__(self, comparator=None):
         self.storage = []
-        self.comparator = comparator
+        if comparator is None:
+            self.comparator = lambda parent, child: parent > child
+        else:
+            self.comparator = comparator
 
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        self._bubble_up(len(self.storage) - 1)
 
     def delete(self):
-        pass
+        self.storage[0], self.storage[-1] = self.storage[-1], self.storage[0]
+        root = self.storage[-1]
+        del self.storage[-1]
+        self._sift_down(0)
+        return root
 
     def get_priority(self):
-        pass
+        return self.storage[0]
 
     def get_size(self):
-        pass
+        return len(self.storage)
 
     def _bubble_up(self, index):
-        pass
+        if index:
+            p = (index - 1) // 2
+            if not self.comparator(self.storage[p], self.storage[index]):
+                self.swap(p, index)
+                self._bubble_up(p)
 
     def _sift_down(self, index):
-        pass
+        l = 2 * index + 1
+        r = 2 * index + 2
+        if l < self.get_size():
+            child = l
+            if r < self.get_size():
+                if self.comparator(self.storage[r], self.storage[l]):
+                    child = r
+
+            if not self.comparator(self.storage[index], self.storage[child]):
+                self.swap(index, child)
+                self._sift_down(child)
+
+    def swap(self, a, b):
+        self.storage[a], self.storage[b] = self.storage[b], self.storage[a]
