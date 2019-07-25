5cf03af931b9ef694bf3c04bf2b2d39a745bd85d
ryanboris <ryan.boris@gmail.com>
2019-07-25 15:38:07 -0400

    :construction: Complete max heap

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,19 +3,39 @@ class Heap:
         self.storage = []
 
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        self._bubble_up(len(self.storage) - 1)
 
     def delete(self):
-        pass
+        removed_item = self.storage[0]
+        del self.storage[0]
+        self._sift_down(0)
+        return removed_item
 
     def get_max(self):
-        pass
+        return self.storage[0]
 
     def get_size(self):
-        pass
+        return len(self.storage)
 
     def _bubble_up(self, index):
-        pass
+        while (index - 1) // 2 >= 0:
+            if self.storage[(index - 1) // 2] < self.storage[index]:
+                self.storage[index], self.storage[(
+                    index - 1) // 2] = self.storage[(index - 1) // 2], self.storage[index]
+            index = (index - 1) // 2
 
     def _sift_down(self, index):
-        pass
+        while index * 2 + 1 <= len(self.storage) - 1:
+            if index * 2 + 2 > len(self.storage) - 1:
+                maxSize = index * 2 + 1
+            elif self.storage[index * 2 + 1] > self.storage[index * 2 + 2]:
+                maxSize = index * 2 + 1
+            else:
+                maxSize = index * 2 + 2
+            if self.storage[maxSize] > self.storage[index]:
+                self.swap(maxSize, index)
+            index = maxSize
+
+    def swap(self, a, b):
+        self.storage[a], self.storage[b] = self.storage[b], self.storage[a]
